c92448cf9db3f8344e16e920961dbed5fc7db2c5
Cullen Jett <cullenjett@gmail.com>
2024-04-17 15:09:11 -0600

    CheckboxField component

--- app/components/forms.tsx
+++ app/components/forms.tsx
@@ -1,5 +1,7 @@
+import { useInputControl } from '@conform-to/react';
 import { useId } from 'react';
 
+import { Checkbox } from '~/components/ui/checkbox';
 import { Input } from '~/components/ui/input';
 import { Label } from '~/components/ui/label';
 import { cn } from '~/lib/styles';
@@ -45,6 +47,80 @@ export function Field({
         aria-describedby={errorId || helpTextId}
         {...inputProps}
       />
+
+      {helpTextId && (
+        <p
+          id={helpTextId}
+          className="text-xs font-medium text-muted-foreground"
+        >
+          {helpText}
+        </p>
+      )}
+
+      {errorId && <FieldErrors id={errorId} errors={errors} />}
+    </div>
+  );
+}
+
+interface CheckboxFieldProps {
+  className?: string;
+  errors?: string[];
+  helpText?: React.ReactNode;
+  checkboxProps: Omit<React.ComponentProps<typeof Checkbox>, 'type'> & {
+    name: string;
+    form: string;
+    value?: string;
+  };
+  label?: React.ReactNode;
+}
+
+export function CheckboxField({
+  className,
+  errors,
+  helpText,
+  checkboxProps,
+  label
+}: CheckboxFieldProps) {
+  const { key, defaultChecked, ...rest } = checkboxProps;
+  const checkedValue = checkboxProps.value ?? 'on';
+  const input = useInputControl({
+    key,
+    name: checkboxProps.name,
+    formId: checkboxProps.form,
+    initialValue: defaultChecked ? checkedValue : undefined
+  });
+  const fallbackId = useId();
+  const id = checkboxProps.id ?? fallbackId;
+  const errorId = errors?.length ? `${id}-error` : undefined;
+  const helpTextId = helpText ? `${id}-help-text` : undefined;
+
+  return (
+    <div className={cn('grid gap-2', className)}>
+      <div className="flex items-center gap-2">
+        <Checkbox
+          {...rest}
+          id={id}
+          aria-invalid={errorId ? true : undefined}
+          aria-describedby={errorId || helpTextId}
+          checked={input.value === checkedValue}
+          onCheckedChange={(state) => {
+            input.change(state.valueOf() ? checkedValue : '');
+            checkboxProps.onCheckedChange?.(state);
+          }}
+          onFocus={(event) => {
+            input.focus();
+            checkboxProps.onFocus?.(event);
+          }}
+          onBlur={(event) => {
+            input.blur();
+            checkboxProps.onBlur?.(event);
+          }}
+          type="button"
+        />
+        <Label htmlFor={id} variant={errorId ? 'destructive' : 'default'}>
+          {label}
+        </Label>
+      </div>
 
       {helpTextId && (
         <p
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
       center: true,
       padding: '2rem',
       screens: {
-        '2xl': '1280px'
+        '2xl': '1440px'
       }
     },
     extend: {
